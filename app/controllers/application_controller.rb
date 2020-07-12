63921c5d6f0e4c7741dd7cfd2f65741a54848aa6
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-12 17:29:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    elsif
+      User.create(:username => params[:username], :password => params[:password])
+      redirect "/login"
+    end
   end
-
+  
   get '/account' do
     @user = User.find(session[:user_id])
     erb :account
@@ -32,7 +36,20 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    # @user = User.find_by(:username => params[:username])
+    # if user && @user.authenticate(params[:password])
+    #   session[:user_id] = @user.id 
+    #   redirect "/account"
+    # else 
+    #   redirect "/failure"
+    # end 
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
@@ -55,3 +72,4 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+
